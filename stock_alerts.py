d9d8d0e8880ff175017cc3eca8fc4c2eac129f7c
shri816 <shri.kadu13@gmail.com>
2025-01-18 12:48:39 +0100

    Update stock_alerts.py

--- stock_alerts.py
+++ stock_alerts.py
@@ -51,7 +51,7 @@ def send_slack_message(webhook_url, trends):
 
 if __name__ == "__main__":
     # Replace with your Slack webhook URL
-    SLACK_WEBHOOK_URL = "xoxb-8299778381476-8309981985793-2CuRvS7ufTpY9BMbzOepjYZ6?"
+    SLACK_WEBHOOK_URL = "https://xoxb-8299778381476-8309981985793-2CuRvS7ufTpY9BMbzOepjYZ6"
     
     try:
         trends = get_nifty50_trends()
